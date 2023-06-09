b04903eeacbacbb2d047336b07a9e8600ce3879c
nvh-gel <nvhien2703@outlook.com>
2023-06-09 10:00:39 +0700

    init project

---
+++ src/test/java/com/eden/backendcore/BackendCoreApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eden.backendcore;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
